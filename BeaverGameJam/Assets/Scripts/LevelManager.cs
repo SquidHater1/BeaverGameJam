5628abeaeee1af74afda8c4ce7404a9e0eb4871f
SquidHater1 <smithr01@yahoo.com>
2021-04-17 03:21:37 -0700

    Added respawning

--- BeaverGameJam/Assets/Scripts/LevelManager.cs
+++ BeaverGameJam/Assets/Scripts/LevelManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public GameObject respawnPointP1;
+    public GameObject respawnPointP2;
+    public GameObject player1;
+    public GameObject player2;
+    
+
     void Start()
     {
         
@@ -17,5 +23,7 @@ public class LevelManager : MonoBehaviour
     public void RespawnPlayers()
     {
         Debug.Log("Players Respawn");
+        player1.transform.position = respawnPointP1.transform.position;
+        player2.transform.position = respawnPointP2.transform.position;
     }
 }
